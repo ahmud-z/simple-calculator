1e657a50b9a947bbe96a8865ab21c0b240e90d81
Ahmudul Hossain <ahmudulhossain.n5@gmail.com>
2024-04-30 13:37:38 +0600

    updated

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 const toggle = document.getElementById('toggleDark');
 const lightLink = document.getElementById('light');
-const themeName = document.getElementById('themeName');
 
 toggle.addEventListener('click', function () {
 
@@ -8,11 +7,9 @@ toggle.addEventListener('click', function () {
 
     if (this.classList.toggle('bi-brightness-high')) {
         lightLink.href = 'style.css';
-        themeName.innerHTML='Day';
         body.style.transition = '1s';
     } else {
         lightLink.href = 'dark.css';
-        themeName.innerHTML='Night';
         body.style.transition = '1s';
     }
 
